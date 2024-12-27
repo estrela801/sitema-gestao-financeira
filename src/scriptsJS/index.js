81a1b868aac424f2d208ae34d23ec8a29f2c015e
estrela801 <rohenri0808@gmail.com>
2024-12-27 14:18:03 -0300

    feat: creating script for the page

--- src/scriptsJS/index.js
+++ src/scriptsJS/index.js
@@ -1,18 +1,33 @@
 "use strict";
-const clicked = (parameter, tipoEvento) => {
-    document.addEventListener(tipoEvento, () => {
-        const economiaElement = document.querySelector('.eco-max');
-        if (economiaElement) {
-            const economia = parseFloat(economiaElement.value);
-            console.log("Valor encontrado", economia);
-        }
-        else {
-            console.log('o valor de economia nao existe');
+const valorEconomia = document.querySelector('.valorEconomia');
+const receita = document.querySelector('.receita');
+const tempoEconomia = document.querySelector('.tempo');
+const diaMesAno = document.querySelector('.dma');
+function resposta(texto) {
+    const resposta = document.querySelector('.resposta');
+    resposta.innerHTML = texto;
+}
+function calculaEconomia(valor, receita, tempo, dma) {
+    console.log("testasdasde");
+    if (receita === 0)
+        return console.log("Sua receita não pode ser 0, hipócrita");
+    const valorDeEconomia = valor / tempo;
+    const TempoDeEconomia = valorDeEconomia / tempo;
+    resposta(`Voce precisa economizar ${valorDeEconomia.toFixed(2)} no tempo de ${tempo} ${dma} voce precisa guardar ${TempoDeEconomia.toFixed(2)}`);
+}
+const btn = document.querySelector(".btn");
+if (btn) {
+    btn.addEventListener("click", () => {
+        console.log("clicado");
+        if (valorEconomia && receita && tempoEconomia) {
+            const valor = parseFloat(valorEconomia.value);
+            const receitaa = parseFloat(receita.value);
+            const tempo = parseFloat(tempoEconomia.value);
+            const dma = diaMesAno.value;
+            if (valor && receitaa && tempo && dma)
+                calculaEconomia(valor, receitaa, tempo, dma);
+            else
+                return resposta("por favor complete as informações");
         }
     });
-};
-const btn = document.querySelector('.btn');
-if (btn)
-    clicked(btn, "click");
-else
-    console.log('btn nao encontrado');
+}
--- src/scriptsTS/index.ts
+++ src/scriptsTS/index.ts
@@ -1,17 +1,38 @@
-const clicked = (parameter:HTMLElement, tipoEvento:string)=>{
-    document.addEventListener(tipoEvento, ()=>{
-        const economiaElement = document.querySelector('.eco-max') as HTMLInputElement
-        if(economiaElement){
-            const economia:Number = parseFloat(economiaElement.value)
-            console.log("Valor encontrado", economia);
+const valorEconomia = document.querySelector('.valorEconomia') as HTMLInputElement
+const receita = document.querySelector('.receita')  as HTMLInputElement
+const tempoEconomia= document.querySelector('.tempo')  as HTMLInputElement
+const diaMesAno = document.querySelector('.dma') as HTMLInputElement
+
+
+
+function resposta(texto:string){
+    const resposta = document.querySelector('.resposta') as HTMLElement
+    resposta.innerHTML = texto
+}
+function calculaEconomia(valor:number,receita:number,tempo:number, dma:string){
+    console.log("testasdasde");
+    
+    if(receita === 0 ) return console.log("Sua receita não pode ser 0, hipócrita");
+    const valorDeEconomia = valor/tempo
+    const TempoDeEconomia = valorDeEconomia / tempo
+    resposta(`Voce precisa economizar ${valorDeEconomia.toFixed(2)} no tempo de ${tempo} ${dma} voce precisa guardar ${TempoDeEconomia.toFixed(2)}`);
+}
+
+const btn = document.querySelector(".btn")
+
+if(btn){
+    btn.addEventListener("click", ()=>{
+        console.log("clicado");
+        
+        if(valorEconomia && receita && tempoEconomia){
+            const valor = parseFloat(valorEconomia.value)
+            const receitaa = parseFloat(receita.value)
+            const tempo = parseFloat(tempoEconomia.value)
+            const dma =  diaMesAno.value
             
-        }else{
-            console.log('o valor de economia nao existe');  
+            if(valor && receitaa && tempo && dma)
+             calculaEconomia(valor, receitaa, tempo, dma)
+            else return resposta("por favor complete as informações")
         }
     })
 }
-const btn = document.querySelector('.btn') as HTMLElement
-if(btn)
-clicked(btn, "click")
-else
-console.log('btn nao encontrado');
