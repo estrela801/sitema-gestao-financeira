e3770db9fed25d62bb6003fa4815da9f857a172e
estrela801 <rohenri0808@gmail.com>
2025-01-13 23:17:19 -0300

    slc muita coisa: comecei o express e criei rotas para testes defini algumas configurações básicas; instalei bibliotecas iniciais para o backend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,29 @@
 // criando o backend do projeto com node
+const express = require('express')
+const path = require("path")
+const app = express()
+const PORT = 3000
+app.use(express.static(path.join(__dirname, "../../frontend/essentials")))
+
+const Page = (page)=>{
+    const filePath = path.join(__dirname, `../../frontend/essentials/views/${page}`)
+    return filePath
+    
+}
+
+app.get("/", (req,res)=>{
+    const page = Page('index.html')
+    res.sendFile(page, (err)=>{
+        if(err){
+            console.error(err)
+            res.status(500).send('ero ao cvarregar')
+        }
+        
+    }) 
+})
+app.get('/sla', (req,res)=>{
+    res.send("sla")
+})
+app.listen(PORT,()=>{
+    console.log(`servidor rodando na porta ${PORT}`)
+})
